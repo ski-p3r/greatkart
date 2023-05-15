efa19d5eb47a9f892924c3623149a6447df23b6b
Mubarek Shikur <manmuba90@gmail.com>
2023-05-15 13:56:36 +0300

    Update urls.py

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
